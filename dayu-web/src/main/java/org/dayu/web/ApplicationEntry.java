c3e021c7468ea9dc5aa5f9ddb485efbda32a570f
Sean Liu <wendaoheri@gmail.com>
2019-08-03 17:33:38 +0800

    add external conf to project

--- dayu-web/src/main/java/org/dayu/web/ApplicationEntry.java
+++ dayu-web/src/main/java/org/dayu/web/ApplicationEntry.java
@@ -2,11 +2,10 @@ package org.dayu.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -15,10 +14,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @Configuration
 @ComponentScan("org.dayu")
-@EntityScan(basePackages = "org.dayu")
 @EnableCaching
 @EnableScheduling
-@EnableJpaRepositories(basePackages = "org.dayu")
+@EnableJms
 public class ApplicationEntry {
 
   public static void main(String[] args) {
